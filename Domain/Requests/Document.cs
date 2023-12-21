eec21f5965c5cd58a81a41689c86d72eff4c988e
User#None <issa_mansur@mail.ru>
2023-12-21 16:05:59 +0200

    Relocate `Document` from `VO` to `Requests`

--- Domain/Requests/Document.cs
+++ Domain/Requests/Document.cs
@@ -1,27 +1,24 @@
 using System.Data;
 using practiceASPNET.Utils;
 
-namespace practiceASPNET.Domain;
+namespace practiceASPNET.Domain.Requests;
 
 public class Document
 {
-    public Guid Id { get; init; }
     public string Name { get; private set; }
     public string Email { get; private set; }
 
-    public Document(Guid id, string name, string email)
+    public Document(string name, string email)
     {
-        Validator.IsValidGuid(id);
         Validator.IsValidName(name);
         Validator.IsValidEmail(email);
 
-        Id = id;
         Name = name;
         Email = email;
     }
 
     static public Document Create(string name, string email)
     {
-        return new Document(Guid.NewGuid(), name, email);
+        return new Document(name, email);
     }
 }
